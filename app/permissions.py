002bdf53a06fb7e90eca280140d90bac27f8f253
FeverCode <giddylancs.gl@gmail.com>
2022-07-10 20:21:23 +0300

    Updated permissions

--- app/permissions.py
+++ app/permissions.py
@@ -4,4 +4,4 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        return obj.user == request.user
